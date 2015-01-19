6e2097088cb67ec3f2380ebfbf02ab16ac3f8e6d
Akash Bhardwaj <akashbdj@gmail.com>
2015-01-19 15:32:56 +0530

    Add a new player feature

--- Leaderboard.js
+++ Leaderboard.js
@@ -1,7 +1,19 @@
 PlayerList = new Meteor.Collection("player");
 
 if (Meteor.isClient) {
+  
   Template.leaderboard.player = function(){
     return PlayerList.find();
   };
+
+  Template.newplayer.events({
+    'click #create' : function(e,t){
+      var name = t.find("#Pname").value;
+      var score = t.find("#Pscore").value;
+      PlayerList.insert({name: name, score: score });
+      name = "";
+      score = "";
+    }
+  });
+
 }
