b595976dcaf25af3b927316c521d62d3a819198e
Akash Bhardwaj <akashbdj@gmail.com>
2015-01-19 18:38:33 +0530

    give points, delete player and sorting

--- Leaderboard.js
+++ Leaderboard.js
@@ -3,16 +3,41 @@ PlayerList = new Meteor.Collection("player");
 if (Meteor.isClient) {
   
   Template.leaderboard.player = function(){
-    return PlayerList.find();
+    return PlayerList.find({}, {sort: {score: -1, name: 1}});
   };
+
+  Template.leaderboard.selected = function(){
+    var selectedPlayer = Session.get('selectedPlayer');
+    var playerId = this._id;
+    if(selectedPlayer == playerId){
+    return 'selected';
+    }
+  };
+
+  Template.leaderboard.events({
+    'click li.player': function(e, t){
+      Session.set('selectedPlayer', this._id);
+    },
+    'click #addScore' : function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayerList.update(
+        {_id: selectedPlayer},
+        {$inc: {score: 5}}
+      );
+    },
+    'click span.delete' : function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayerList.remove(selectedPlayer);
+    }
+  });
 
   Template.newplayer.events({
     'click #create' : function(e,t){
-      var name = t.find("#Pname").value;
-      var score = t.find("#Pscore").value;
-      PlayerList.insert({name: name, score: score });
-      name = "";
-      score = "";
+      var name = t.find("#Pname");
+      var score = t.find("#Pscore");
+      PlayerList.insert({name: name.value, score: parseInt(score.value) });
+      name.value = "";
+      score.value = "";
     }
   });
 
